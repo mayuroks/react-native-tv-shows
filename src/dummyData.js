517602f0e6d407dccf3d4d5349dbad789b50ec82
Mayur Rokade <mayur@grexit.com>
2019-08-19 02:01:57 +0530

    dummy data updated

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,5 +1,47 @@
 export const data = [
     {
+        title: "Arrow",
+        imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/257655-5.jpg",
+        episodes: [
+            {
+                title: "Episode 1",
+                rating: "9.2",
+                pricing: "$5.32",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/257655-6.jpg",
+            },
+            {
+                title: "Episode 2",
+                rating: "9.7",
+                pricing: "$6.11",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/257655-6.jpg",
+            },
+            {
+                title: "Episode 3",
+                rating: "8.9",
+                pricing: "$6.24",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/257655-6.jpg",
+            },
+            {
+                title: "Episode 4",
+                rating: "8.3",
+                pricing: "$6.60",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/257655-6.jpg",
+            },
+            {
+                title: "Episode 5",
+                rating: "8.1",
+                pricing: "$5.72",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/257655-6.jpg",
+            },
+            {
+                title: "Episode 6",
+                rating: "9.2",
+                pricing: "$5.13",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/257655-6.jpg",
+            }
+        ]
+    },
+    {
         title: "Breaking Bad",
         imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-1.jpg",
         episodes: [
@@ -7,249 +49,122 @@ export const data = [
                 title: "Episode 1",
                 rating: "9.2",
                 pricing: "$5.32",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-8.jpg",
             },
             {
                 title: "Episode 2",
                 rating: "9.7",
                 pricing: "$6.11",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-8.jpg",
             },
             {
                 title: "Episode 3",
                 rating: "8.9",
                 pricing: "$6.24",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-8.jpg",
             },
             {
                 title: "Episode 4",
                 rating: "8.3",
                 pricing: "$6.60",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-8.jpg",
             },
             {
                 title: "Episode 5",
                 rating: "8.1",
                 pricing: "$5.72",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-8.jpg",
             },
             {
                 title: "Episode 6",
                 rating: "9.2",
                 pricing: "$5.13",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-8.jpg",
             }
         ]
     },
     {
-        title: "Breaking Bad",
-        imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+        title: "Game of Thrones",
+        imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/121361-4.jpg",
         episodes: [
             {
                 title: "Episode 1",
                 rating: "9.2",
                 pricing: "$5.32",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/121361-2.jpg",
             },
             {
                 title: "Episode 2",
                 rating: "9.7",
                 pricing: "$6.11",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/121361-2.jpg",
             },
             {
                 title: "Episode 3",
                 rating: "8.9",
                 pricing: "$6.24",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/121361-2.jpg",
             },
             {
                 title: "Episode 4",
                 rating: "8.3",
                 pricing: "$6.60",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/121361-2.jpg",
             },
             {
                 title: "Episode 5",
                 rating: "8.1",
                 pricing: "$5.72",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/121361-2.jpg",
             },
             {
                 title: "Episode 6",
                 rating: "9.2",
                 pricing: "$5.13",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/121361-2.jpg",
             }
         ]
     },
     {
-        title: "Breaking Bad",
-        imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+        title: "The Walking Dead",
+        imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/153021-4.jpg",
         episodes: [
             {
                 title: "Episode 1",
                 rating: "9.2",
                 pricing: "$5.32",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/153021-6.jpg",
             },
             {
                 title: "Episode 2",
                 rating: "9.7",
                 pricing: "$6.11",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/153021-6.jpg",
             },
             {
                 title: "Episode 3",
                 rating: "8.9",
                 pricing: "$6.24",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/153021-6.jpg",
             },
             {
                 title: "Episode 4",
                 rating: "8.3",
                 pricing: "$6.60",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/153021-6.jpg",
             },
             {
                 title: "Episode 5",
                 rating: "8.1",
                 pricing: "$5.72",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/153021-6.jpg",
             },
             {
                 title: "Episode 6",
                 rating: "9.2",
                 pricing: "$5.13",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
+                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/153021-6.jpg",
             }
         ]
     },
-    {
-        title: "Breaking Bad",
-        imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-        episodes: [
-            {
-                title: "Episode 1",
-                rating: "9.2",
-                pricing: "$5.32",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 2",
-                rating: "9.7",
-                pricing: "$6.11",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 3",
-                rating: "8.9",
-                pricing: "$6.24",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 4",
-                rating: "8.3",
-                pricing: "$6.60",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 5",
-                rating: "8.1",
-                pricing: "$5.72",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 6",
-                rating: "9.2",
-                pricing: "$5.13",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            }
-        ]
-    },
-    {
-        title: "Breaking Bad",
-        imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-        episodes: [
-            {
-                title: "Episode 1",
-                rating: "9.2",
-                pricing: "$5.32",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 2",
-                rating: "9.7",
-                pricing: "$6.11",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 3",
-                rating: "8.9",
-                pricing: "$6.24",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 4",
-                rating: "8.3",
-                pricing: "$6.60",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 5",
-                rating: "8.1",
-                pricing: "$5.72",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 6",
-                rating: "9.2",
-                pricing: "$5.13",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            }
-        ]
-    },
-    {
-        title: "Breaking Bad",
-        imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-        episodes: [
-            {
-                title: "Episode 1",
-                rating: "9.2",
-                pricing: "$5.32",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 2",
-                rating: "9.7",
-                pricing: "$6.11",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 3",
-                rating: "8.9",
-                pricing: "$6.24",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 4",
-                rating: "8.3",
-                pricing: "$6.60",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 5",
-                rating: "8.1",
-                pricing: "$5.72",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            },
-            {
-                title: "Episode 6",
-                rating: "9.2",
-                pricing: "$5.13",
-                imageUrl: "https://www.thetvdb.com/banners/_cache/fanart/original/81189-3.jpg",
-            }
-        ]
-    },
-
 ]
